bda7441b4392141cb0902f91330fb2c292abd243
David Fletcher <dfletch1@binghamton.edu>
2019-09-16 17:33:09 -0400

    Things are very broken right now

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.*;
@@ -14,15 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GraphView graph = findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+        final EditText slope = (EditText) findViewById(R.id.slope);
+        final EditText intercept = (EditText) findViewById(R.id.intercept);
+        final GraphView graph = findViewById(R.id.graph);
+        final LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double x, y, cross;
+                x = -100.0;
+                cross = Double.parseDouble(intercept.getText().toString());
+                for(int i = 0; i < 2000; i++){
+                    double slope_num = Double.parseDouble(slope.getText().toString());
+                    y = slope_num * x + cross;
+                    series.appendData(new DataPoint(x, y), true, 2000);
+                    x += 0.1;
+                }
+                graph.addSeries(series);
+            }
         });
-        graph.addSeries(series);
     }
 
 
